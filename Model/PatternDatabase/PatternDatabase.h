6b3ccf504c08f9242708098a59cd1dd4ab9f3fdb
shashank3233 <shashank6480@kgpian.iitkgp.ac.in>
2024-06-27 16:14:14 +0530

    RS

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,19 @@
+//
+// Created by Shashank Shekhar on 24-05-2024.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+#endif //RUBIKS_CUBE_SOLVER_PATTERNDATABASE_H
